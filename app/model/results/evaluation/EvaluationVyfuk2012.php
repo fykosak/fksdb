9ae5611832a52a329ef984a631c96fb61afb75e3
Michal Koutný <xm.koutny@gmail.com>
2013-01-22 23:08:28 +0100

    [results] separate category for unidentified students in Výfuk

---
+++ app/model/results/evaluation/EvaluationVyfuk2012.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Introduced in Výfuk 2011 (1st official year).
+ *
+ * @author Michal Koutný <michal@fykos.cz>
+ */
+class EvaluationVyfuk2012 implements IEvaluationStrategy {
+
+    private $categories = null;
+
+    public function getCategories() {
+        if ($this->categories == null) {
+            $this->categories = array(
+                new ModelCategory(ModelCategory::CAT_ES_6),
+                new ModelCategory(ModelCategory::CAT_ES_7),
+                new ModelCategory(ModelCategory::CAT_ES_8),
+                new ModelCategory(ModelCategory::CAT_ES_9),
+                new ModelCategory(ModelCategory::CAT_UNK),
+            );
+        }
+        return $this->categories;
+    }
+
+    public function categoryToStudyYears($category) {
+        switch ($category->id) {
+            case ModelCategory::CAT_ES_6:
+                return 6;
+            case ModelCategory::CAT_ES_7:
+                return 7;
+            case ModelCategory::CAT_ES_8:
+                return 8;
+            case ModelCategory::CAT_ES_9:
+                return 9;
+            case ModelCategory::CAT_UNK:
+                return null;
+            default:
+                throw new Nette\InvalidArgumentException('Invalid category ' . $category->id);
+                break;
+        }
+    }
+
+    public function getPointsColumn($task) {
+        return "s.raw_points";
+    }
+
+    public function getSumColumn() {
+        return "s.raw_points";
+    }
+
+    public function getTaskPoints($task, \ModelCategory $category) {
+        return $task->points;
+    }
+    
+    public function getTaskPointsColumn(\ModelCategory $category) {
+        return 'IF(s.raw_points IS NOT NULL, t.points, NULL)';
+    }
+}
