507b134984b83abe161f2d3af008b01f8a845171
Michal Koutný <xm.koutny@gmail.com>
2013-07-08 21:02:47 +0200

    druhá verze pohledu pro GoodData

--- sql/views.sql
+++ sql/views.sql
@@ -10,7 +10,8 @@ CREATE OR REPLACE VIEW v_task_stats as
 CREATE OR REPLACE VIEW v_gooddata as (
 -- Reseni;Rocnik;Serie;CisloUlohy;Uloha;MaxBodu;Bodu;Resitel;Rokmaturity;Pohlavi;Skola;Mesto;Stat
 
-select s.submit_id AS Reseni, t.year AS Rocnik, t.series AS Serie, t.tasknr AS CisloUlohy, t.label AS Uloha, t.points AS MaxBodu, s.raw_points AS Bodu, ct.person_id AS Resitel, 
+select s.submit_id AS Reseni, t.year AS Rocnik, t.series AS Serie, t.tasknr AS CisloUlohy, t.label AS Uloha, t.points AS MaxBodu, s.raw_points AS Bodu,
+IF(p.display_name is null, concat(p.other_name, ' ', p.family_name), p.display_name) AS Resitel, 
 (IF(ct.contest_id = 1, 1991 + ct.year, 2015 + ct.year) - IF(ct.study_year between 1 and 4, ct.study_year, ct.study_year - 9) ) AS RokMaturity,
 p.gender AS Pohlavi, sch.name_abbrev AS Skola, scha.city AS Mesto, reg.country_iso AS Stat, cst.name AS Seminar
 
