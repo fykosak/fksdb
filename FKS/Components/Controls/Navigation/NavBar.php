e789507a4dae03a5a0df5950c697bf84ac3c69c7
Michal Koutný <xm.koutny@gmail.com>
2013-10-28 04:36:32 +0100

    NavBar control

--- FKS/Components/Controls/Navigation/NavBar.php
+++ FKS/Components/Controls/Navigation/NavBar.php
@@ -2,7 +2,14 @@
 
 namespace FKS\Components\Controls\Navigation;
 
+use Nette\Application\ForbiddenRequestException;
+use Nette\Application\PresenterFactory;
 use Nette\Application\UI\Control;
+use Nette\Application\UI\InvalidLinkException;
+use Nette\Application\UI\Presenter;
+use Nette\InvalidArgumentException;
+use ReflectionClass;
+use ReflectionMethod;
 
 /**
  * Due to author's laziness there's no class doc (or it's self explaining).
@@ -11,20 +18,165 @@ use Nette\Application\UI\Control;
  */
 class NavBar extends Control {
 
-    public function isActive($nodeId) {
-        
+    private $nodes = array();
+    private $nodeChildren = array();
+
+    /**
+     * @var PresenterFactory
+     */
+    private $presenterFactory;
+
+    /**
+     * @var array of Presenter
+     */
+    private $presenters = array();
+    private $structure;
+
+    function __construct(PresenterFactory $presenterFactory) {
+        $this->presenterFactory = $presenterFactory;
     }
 
-    public function isVisible($nodeId) {
-        
+    public function getNode($nodeId) {
+        return $this->nodes[$nodeId];
+    }
+
+    public function isActive($node) {
+        $result = false;
+        if (isset($node->linkPresenter)) {
+            $presenter = $this->getPresenter();
+            try {
+                $this->createLink($presenter, $node);
+            } catch (InvalidLinkException $e) {
+                /* empty */
+            }
+            $result = $presenter->getLastCreatedRequestFlag("current");
+        } else {
+            $result = false;
+        }
+
+        if ($result) {
+            return true;
+        }
+        // try children
+        if (!isset($this->nodeChildren[$node->nodeId])) {
+            return false;
+        }
+        foreach ($this->nodeChildren[$node->nodeId] as $childId) {
+            if ($this->isActive($this->nodes[$childId])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function isVisible($node) {
+        if (isset($node->visible)) {
+            return $node->visible;
+        }
+
+        if (isset($node->linkPresenter)) {
+            $presenter = $this->preparePresenter($node->linkPresenter, $node->linkAction, $node->linkParams);
+            try {
+                $presenter->checkRequirements($presenter->getReflection());
+                return true;
+            } catch (ForbiddenRequestException $e) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public function getTitle($node) {
+        if (isset($node->title)) {
+            return $node->title;
+        }
+        if (isset($node->linkPresenter)) {
+            $presenter = $this->preparePresenter($node->linkPresenter, $node->linkAction, $node->linkParams);
+            return $presenter->getTitle();
+        }
+    }
+
+    public function getLink($node) {
+        if (isset($node->link)) {
+            return $node->link;
+        }
+        if (isset($node->linkPresenter)) {
+            return $this->createLink($this->getPresenter(), $node);
+        }
+    }
+
+    public function setStructure($structure) {
+        $this->structure = $structure;
     }
 
     public function createNode($nodeId, $arguments) {
-        
+        $node = (object) $arguments;
+        $this->nodes[$nodeId] = $node;
     }
-    
+
     public function addParent($idChild, $idParent) {
-        
+        if (!isset($this->nodeChildren)) {
+            $this->nodeChildren[$idParent] = array();
+        }
+        $this->nodeChildren[$idParent][] = $idChild;
+    }
+
+    public function render() {
+        $template = $this->getTemplate();
+        $template->setFile(__DIR__ . DIRECTORY_SEPARATOR . 'NavBar.latte');
+
+        $template->nodes = $this->structure;
+
+        $template->render();
+    }
+
+    private function createLink($presenter, $node) {
+        $linkedPresenter = $this->preparePresenter($node->linkPresenter, $node->linkAction, $node->linkParams);
+        $method = $linkedPresenter->publicFormatActionMethod($node->linkAction);
+
+        $linkParams = array();
+        $rc = new ReflectionClass($linkedPresenter);
+        if ($rc->hasMethod($method)) {
+            $rm = new ReflectionMethod($linkedPresenter, $method);
+            foreach ($rm->getParameters() as $param) {
+                $name = $param->getName();
+                $linkParams[$name] = $node->linkParams[$name];
+            }
+        }
+
+        return $presenter->link(':' . $node->linkPresenter . ':' . $node->linkAction, $linkParams);
+    }
+
+    /**
+     * 
+     * @param string $presenterName
+     * @return Presenter
+     */
+    private function getOuterPresener($presenterName) {
+        if (!isset($this->presenters[$presenterName])) {
+            $this->presenters[$presenterName] = $this->presenterFactory->createPresenter($presenterName);
+        }
+        return $this->presenters[$presenterName];
+    }
+
+    private function preparePresenter($presenter, $action, $providedParams) {
+        $providedParams = $providedParams ? : array();
+        $presenter = $this->getOuterPresener($presenter);
+        if (!$presenter instanceof INavigablePresenter) {
+            throw new InvalidArgumentException("Presenter must be instance of INavigablePresenter.");
+        }
+
+        $params = $this->getPresenter()->getParameter(); // by default inherit parameters of the calling presenter -- is this alright?
+        unset($params[Presenter::ACTION_KEY]);
+        foreach ($providedParams as $key => $value) {
+            $params[$key] = $value;
+        }
+        $presenter->loadState($params);
+        $presenter->changeAction($action);
+        $presenter->setView($presenter->getView()); // to force update the title    
+
+        return $presenter;
     }
 
 }
--- FKS/Config/Extensions/NavigationExtension.php
+++ FKS/Config/Extensions/NavigationExtension.php
@@ -11,6 +11,8 @@ use Nette\Config\CompilerExtension;
  */
 class NavigationExtension extends CompilerExtension {
 
+    private $createdNodes = array();
+
     public function loadConfiguration() {
         parent::loadConfiguration();
 
@@ -19,12 +21,61 @@ class NavigationExtension extends CompilerExtension {
             'nodes' => array(),
             'structure' => array(),
         ));
-        $navbar = $builder->addDefinition($this->prefix('navbar'))
-                        ->setClass('FKS\Components\Controls\Navigation\Navbar')
-                        ->setShared(FALSE)->setAutowired(FALSE);
+        $navbar = $builder->addDefinition('navbar')
+                ->setClass('FKS\Components\Controls\Navigation\NavBar');
+        $navbar->setShared(true)->setAutowired(true);
+
+
         foreach ($config['nodes'] as $nodeId => $arguments) {
-            $navbar->addSetup('$service->createNode(?, ?);', array($nodeId, $arguments));
+            $this->createNode($navbar, $nodeId, $arguments);
         }
+
+        $this->createFromStructure($config['structure'], $navbar);
+
+
+        $navbar->addSetup('$service->setStructure(?);', array($config['structure']));
+    }
+
+    private function createNode($navbar, $nodeId, $arguments = array()) {
+        if (!isset($arguments['link'])) {
+            $this->parseIdAsLink($nodeId, $arguments);
+        }
+        $this->createdNodes[$nodeId] = 1;
+        $arguments['nodeId'] = $nodeId;
+        $navbar->addSetup('$service->createNode(?, ?);', array($nodeId, $arguments));
+    }
+
+    private function createFromStructure($structure, $navbar, $parent = null) {
+        foreach ($structure as $nodeId => $children) {
+            if (is_array($children)) {
+                if (!isset($this->createdNodes[$nodeId])) {
+                    $this->createNode($navbar, $nodeId);
+                    if ($parent) {
+                        $navbar->addSetup('$service->addParent(?, ?);', array($nodeId, $parent));
+                    }
+                }
+                $this->createFromStructure($children, $navbar, $nodeId);
+            } else if (!is_array($children)) {
+                $nodeId = $children;
+                if (!isset($this->createdNodes[$nodeId])) {
+                    $this->createNode($navbar, $nodeId);
+                    if ($parent) {
+                        $navbar->addSetup('$service->addParent(?, ?);', array($nodeId, $parent));
+                    }
+                }
+            }
+        }
+    }
+
+    private function parseIdAsLink($nodeId, &$arguments) {
+        $FQAction = str_replace('.', ':', $nodeId);
+        $a = strrpos($FQAction, ':');
+        $presenterName = substr($FQAction, 0, $a);
+        $action = substr($FQAction, $a + 1);
+        $arguments['linkPresenter'] = $presenterName;
+        $arguments['linkAction'] = $action;
+        $arguments['linkParams'] = isset($arguments['params']) ? $arguments['params'] : null;
+        unset($arguments['params']);
     }
 
 }
