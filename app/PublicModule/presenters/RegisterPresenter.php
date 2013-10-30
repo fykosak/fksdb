93eaa41fb64cceb507f0b7d9044e8744c432c6d9
Michal Koutný <xm.koutny@gmail.com>
2013-10-30 23:00:37 +0100

    fix in namespace resolution

--- app/PublicModule/presenters/RegisterPresenter.php
+++ app/PublicModule/presenters/RegisterPresenter.php
@@ -2,7 +2,7 @@
 
 namespace PublicModule;
 
-use BasePresenter;
+use BasePresenter as CoreBasePresenter;
 use FKSDB\Components\Forms\Factories\AddressFactory;
 use FKSDB\Components\Forms\Factories\ContestantFactory;
 use FKSDB\Components\Forms\Factories\LoginFactory;
@@ -54,7 +54,7 @@ use ServicePostContact;
  * 
  * @author Michal Koutný <michal@fykos.cz>
  */
-class RegisterPresenter extends BasePresenter implements IContestPresenter {
+class RegisterPresenter extends CoreBasePresenter implements IContestPresenter {
 
     const CONT_PERSON = 'person';
     const CONT_PERSON_INFO = 'person_info';
