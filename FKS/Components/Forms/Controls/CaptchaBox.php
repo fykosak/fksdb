4d51160f7158baaa9883081e10fd8677a1c9f66c
Michal Koutný <xm.koutny@gmail.com>
2013-12-05 02:01:52 +0100

    simple CAPTCHA element

---
+++ FKS/Components/Forms/Controls/CaptchaBox.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace FKS\Components\Forms\Controls;
+
+use Nette\Forms\Controls\BaseControl;
+use Nette\Forms\Controls\SelectBox;
+
+/**
+ * Due to author's laziness there's no class doc (or it's self explaining).
+ * 
+ * @author Michal Koutný <michal@fykos.cz>
+ */
+class CaptchaBox extends SelectBox {
+
+    const VALUE_YES = 'xyz';
+    const VALUE_NO = 'pqrt';
+
+    public function __construct() {
+        parent::__construct(_('Jsi robot?'), array(
+            self::VALUE_NO => _('ne'),
+            self::VALUE_YES => _('ano'),
+        ));
+
+        $this->addRule(function(BaseControl $control) {
+                    return $control->getValue() == self::VALUE_NO;
+                }, _('Tento formulář je jenom pro lidi.'));
+
+        $this->setDefaultValue(self::VALUE_YES);
+    }
+
+}
