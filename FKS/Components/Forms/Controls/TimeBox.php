ab3a56e2c4f571b9ed28c9ec13830f1100738783
Michal Koutný <xm.koutny@gmail.com>
2014-01-23 21:32:47 +0100

    added TimeBox

---
+++ FKS/Components/Forms/Controls/TimeBox.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace FKS\Components\Forms\Controls;
+
+use DateTime;
+use Nette\Forms\Controls\TextInput;
+use Nette\Forms\Form;
+
+/**
+ * Due to author's laziness there's no class doc (or it's self explaining).
+ * 
+ * @author Michal Koutný <michal@fykos.cz>
+ */
+class TimeBox extends TextInput {
+
+    const TIME_FORMAT = 'H:i:s';
+
+    public function __construct($label = NULL, $cols = NULL, $maxLength = NULL) {
+        parent::__construct($label, $cols, $maxLength);
+
+        $this->addCondition(Form::FILLED)
+                ->addRule(Form::REGEXP, _('%label očekává hh:mm[:ss].'), '/^[0-2]?\d:[0-5]\d(:[0-5]\d)?$/');
+    }
+
+    public function setValue($value) {
+        if ($value instanceof DateTime) {
+            $value = $value->format(self::TIME_FORMAT);
+        }
+        parent::setValue($value);
+    }
+
+}
