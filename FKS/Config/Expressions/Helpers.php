c3bd60f6c029dc6a4355fda285224b1a403f5bcf
Michal Koutný <xm.koutny@gmail.com>
2014-02-08 11:53:34 +0100

    Neon: support for dynamic expression eval

--- FKS/Config/Expressions/Helpers.php
+++ FKS/Config/Expressions/Helpers.php
@@ -4,7 +4,9 @@ namespace FKS\Config\Expressions;
 
 use Nette\DI\Statement;
 use Nette\Utils\Arrays;
+use ReflectionClass;
 use stdClass;
+use Traversable;
 
 /**
  * Due to author's laziness there's no class doc (or it's self explaining).
@@ -47,7 +49,50 @@ class Helpers {
         }
 
         $class = Arrays::get(self::$semanticMap, $expression->value, $expression->value);
+        if (function_exists($class)) { // workaround for Nette interpretation of entities
+            $class = array('', $class);
+        }
         return new Statement($class, $arguments);
+    }
+
+    /**
+     * Transforms and evalutes the expression during runtime.
+     * 
+     * @param mixed $expression
+     * @return mixed
+     */
+    public static function evalExpression($expression) {
+        if (!$expression instanceof stdClass) {
+            return $expression;
+        }
+
+        $arguments = array();
+        foreach ($expression->attributes as $attribute) {
+            if ($attribute === '...') {
+                continue;
+            }
+            $arguments[] = self::evalExpression($attribute);
+        }
+
+        $entity = Arrays::get(self::$semanticMap, $expression->value, $expression->value);
+        if (function_exists($entity)) {
+            return call_user_func_array($entity, $arguments);
+        } else {
+            $rc = new ReflectionClass($entity);
+            return $rc->newInstanceArgs($arguments);
+        }
+    }
+
+    public static function evalExpressionArray($expressionArray) {
+        if ($expressionArray instanceof Traversable || is_array($expressionArray)) {
+            $result = array();
+            foreach ($expressionArray as $key => $expression) {
+                $result[$key] = self::evalExpressionArray($expression);
+            }
+            return $result;
+        } else {
+            return self::evalExpression($expressionArray);
+        }
     }
 
 }
