109867d76336ef06fe207e3c204f577f1ad9735b
Michal Koutný <xm.koutny@gmail.com>
2014-09-20 22:36:21 +0200

    IEvaluationStrategy: cancelled UNK category

---
+++ app/model/results/evaluation/EvaluationVyfuk2014.php
@@ -0,0 +1,57 @@
+<?php
+
+use Nette\InvalidArgumentException;
+
+/**
+ * Introduced in Výfuk 2014 (4th official year).
+ *
+ * @author Michal Koutný <michal@fykos.cz>
+ */
+class EvaluationVyfuk2014 implements IEvaluationStrategy {
+
+    private $categories = null;
+
+    public function getCategories() {
+        if ($this->categories == null) {
+            $this->categories = array(
+                new ModelCategory(ModelCategory::CAT_ES_6),
+                new ModelCategory(ModelCategory::CAT_ES_7),
+                new ModelCategory(ModelCategory::CAT_ES_8),
+                new ModelCategory(ModelCategory::CAT_ES_9),
+            );
+        }
+        return $this->categories;
+    }
+
+    public function categoryToStudyYears($category) {
+        switch ($category->id) {
+            case ModelCategory::CAT_ES_6:
+                return 6;
+            case ModelCategory::CAT_ES_7:
+                return 7;
+            case ModelCategory::CAT_ES_8:
+                return 8;
+            case ModelCategory::CAT_ES_9:
+                return array(null, 9);
+            default:
+                throw new InvalidArgumentException('Invalid category ' . $category->id);
+                break;
+        }
+    }
+
+    public function getPointsColumn($task) {
+        return "s.raw_points";
+    }
+
+    public function getSumColumn() {
+        return "s.raw_points";
+    }
+
+    public function getTaskPoints($task, \ModelCategory $category) {
+        return $task->points;
+    }
+    
+    public function getTaskPointsColumn(\ModelCategory $category) {
+        return 'IF(s.raw_points IS NOT NULL, t.points, NULL)';
+    }
+}
