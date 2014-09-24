67bc127cd28022738844e4ff7284096c23962496
Michal Koutný <xm.koutny@gmail.com>
2014-09-24 10:48:00 +0200

    [tasks] importing study years

---
+++ app/model/ORM/services/ServiceStudyYear.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @author Michal Koutný <xm.koutny@gmail.com>
+ */
+class ServiceStudyYear extends AbstractServiceSingle {
+
+    protected $tableName = DbNames::TAB_STUDY_YEAR;
+    protected $modelClassName = 'ModelStudyYear';
+
+}
+
