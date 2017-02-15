dfcc7bcb57b8fe17feca3c4e8caab0311d3cad2e
Lukas Timko <lukast@fykos.cz>
2017-02-15 04:03:06 +0100

    [event org] permissions

--- app/Components/Grids/EventOrgsGrid.php
+++ app/Components/Grids/EventOrgsGrid.php
@@ -39,14 +39,21 @@ class EventOrgsGrid extends BaseGrid {
         $this->addButton("edit", _("Upravit"))->setText('Upravit')//todo i18n
             ->setLink(function ($row) use ($that) {
                 return $that->getPresenter()->link('edit', $row->e_org_id);
+            })
+            ->setShow(function($row) use ($presenter) {
+                return $presenter->authorized("edit", ['id' => $row->e_org_id]);
             });
         $this->addButton("delete", _("Smazat"))->setClass('btn btn-xs btn-danger')->setText('Smazat')//todo i18n
-        ->setLink(function ($row) use ($that) {
-            return $that->getPresenter()->link("delete", $row->e_org_id);
-
-        })->setConfirmationDialog(function () {
-            return _("Opravdu smazat organizátora?"); //todo i18n
-        });
+            ->setLink(function ($row) use ($that) {
+                return $that->getPresenter()->link("delete", $row->e_org_id);
+            })
+            ->setShow(function($row) use ($presenter) {
+                return $presenter->authorized("delete", ['id' => $row->e_org_id]);
+            })
+            ->setConfirmationDialog(function () {
+                return _("Opravdu smazat organizátora?"); //todo i18n
+            });
+        
         if ($presenter->authorized('create')) {
             $this->addGlobalButton('add')
                     ->setLabel('Přidat organizátora')
