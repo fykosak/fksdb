ae6f073588e7f21ef139a2df06b9128d2705a9b8
Mišo <miso@fykos.cz>
2018-04-18 23:57:55 +0200

    code cleanup

--- app/Components/Controls/Stalking/EventParticipant.php
+++ app/Components/Controls/Stalking/EventParticipant.php
@@ -3,17 +3,6 @@
 namespace FKSDB\Components\Controls\Stalking;
 
 class EventParticipant extends StalkingComponent {
-    private $mode;
-    /**
-     * @var \ModelPerson;
-     */
-    private $modelPerson;
-
-    public function __construct(\ModelPerson $modelPerson, $mode = null) {
-        parent::__construct();
-        $this->mode = $mode;
-        $this->modelPerson = $modelPerson;
-    }
 
     public function render() {
         $template = $this->template;
