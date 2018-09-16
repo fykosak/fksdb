098b9e28e5fdb00fb9d299e76a0ce6a41acdc257
Mišo <miso@fykos.cz>
2018-09-16 18:16:24 +0200

    remove AJAX class

--- app/Components/Forms/Containers/Models/ReferencedContainer.php
+++ app/Components/Forms/Containers/Models/ReferencedContainer.php
@@ -238,7 +238,7 @@ class ReferencedContainer extends ContainerWithOptions {
         }
         if (!$this->attachedAjax && $obj instanceof Form) {
             $this->attachedAjax = true;
-            $this->getForm()->getElementPrototype()->class[] = self::CSS_AJAX;
+            // $this->getForm()->getElementPrototype()->class[] = self::CSS_AJAX;
         }
     }
 
@@ -293,4 +293,3 @@ class ReferencedContainer extends ContainerWithOptions {
     }
 
 }
-
