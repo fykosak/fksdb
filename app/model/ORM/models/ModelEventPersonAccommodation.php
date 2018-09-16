bf9ccf4da80acee37682c197f6d8860144cdc93e
Mišo <miso@fykos.cz>
2018-09-16 17:28:50 +0200

    update ORM classes

--- app/model/ORM/models/ModelEventPersonAccommodation.php
+++ app/model/ORM/models/ModelEventPersonAccommodation.php
@@ -1,5 +1,15 @@
 <?php
 
-class ModelEventPersonAccommodation extends AbstractModelSingle {
+/**
+ * Class ModelEventPersonAccommodation
+ * @property integer person_id
+ * @property integer event_accommodation_id
+ * @property integer event_person_accommodation_id
+ *
+ */
+class ModelEventPersonAccommodation extends \AbstractModelSingle {
 
+    public function getEventAccommodation() {
+        return ModelEventAccommodation::createFromTableRow($this->event_accommodation);
+    }
 }
