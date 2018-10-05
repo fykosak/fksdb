9cfa65b0bb3d746240c0b81e365abb162bf14a1c
Mišo <miso@fykos.cz>
2018-10-05 20:18:31 +0200

    add tests

---
+++ tests/Events/Accommodation/Accommodation.create.phpt
@@ -0,0 +1,58 @@
+<?php
+
+namespace Events\Accommodation;
+
+use Tester\Assert;
+
+$container = require '../../bootstrap.php';
+
+class AccommodationTest extends AccommodationTestCase {
+
+    public function testRegistration() {
+        $request = $this->createPostRequest([
+            'participant' => [
+                'person_id' => "__promise",
+                'person_id_1' => [
+                    '_c_compact' => " ",
+                    'person' => [
+                        'other_name' => "František",
+                        'family_name' => "Dobrota",
+                    ],
+                    'person_info' => [
+                        'email' => "ksaadaa@kalo.cz",
+                        'id_number' => "1231354",
+                        'born' => "15. 09. 2014",
+                    ],
+                    'post_contact_p' => [
+                        'address' => [
+                            'target' => "jkljhkjh",
+                            'city' => "jkhlkjh",
+                            'postal_code' => "64546",
+                            'country_iso' => "",
+                        ],
+                    ],
+                    'person_accommodation' => [
+                        'matrix' => json_encode(['2018-06-05' => $this->accId]),
+                    ],
+                ],
+                'e_dsef_group_id' => 2,
+                'lunch_count' => 0,
+                'message' => "",
+            ],
+            'privacy' => "on",
+            'c_a_p_t_cha' => "pqrt",
+            '__init__applied' => "Přihlásit účastníka",
+        ]);
+
+        $response = $this->fixture->run($request);
+        Assert::type('Nette\Application\Responses\RedirectResponse', $response);
+
+        Assert::equal('3', $this->connection->fetchColumn('SELECT count(*) FROM event_person_accommodation WHERE event_accommodation_id = ?', $this->accId));
+    }
+
+
+}
+
+
+$testCase = new AccommodationTest($container);
+$testCase->run();
---
+++ tests/Events/Accommodation/AccommodationTestCase.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Events\Accommodation;
+
+
+use Events\EventTestCase;
+use Nette\DateTime;
+use Nette\DI\Container;
+
+abstract class AccommodationTestCase extends EventTestCase {
+    protected $accId;
+    protected $fixture;
+
+    function __construct(Container $container) {
+        parent::__construct($container);
+        $this->setContainer($container);
+    }
+
+    //protected $persons = [];
+
+    protected function setUp() {
+        parent::setUp();
+
+        $this->eventId = $this->createEvent([
+            'event_type_id' => 2,
+            'event_year' => 24,
+            'parameters' => <<<EOT
+EOT
+        ]);
+        $addressId = $this->insert('address', [
+            'target' => 'horná 42',
+            'city' => 'Horná-dolná',
+            'postal_code' => 12345,
+            'region_id' => 3,
+        ]);
+
+        $this->accId = $this->insert('event_accommodation', [
+            'event_id' => $this->eventId,
+            'name' => 'Hotel Test',
+            'price_kc' => 20,
+            'price_eur' => 30,
+            'capacity' => 3,
+            'address_id' => $addressId,
+            'date' => new \DateTime(),
+        ]);
+
+        $this->insert('e_dsef_group', [
+            'e_dsef_group_id' => 2,
+            'event_id' => $this->eventId,
+            'name' => 'Alpha',
+            'capacity' => 4
+        ]);
+
+
+        $this->fixture = $this->createPresenter('Public:Application');
+        $this->mockApplication();
+
+
+        $this->persons[] = $this->createPerson('Paní', 'Bílá',
+            [
+                'email' => 'bila-acc@hrad.cz',
+                'born' => DateTime::from('2000-01-01'),
+            ]);
+        $this->insert('event_participant', [
+            'person_id' => end($this->persons),
+            'event_id' => $this->eventId,
+            'status' => 'applied',
+        ]);
+        $this->insert('event_person_accommodation', [
+            'person_id' => end($this->persons),
+            'event_accommodation_id' => $this->accId,
+        ]);
+
+
+        $this->persons[] = $this->createPerson('Paní', 'Bílá II.',
+            [
+                'email' => 'bila2-acc@hrad.cz',
+                'born' => DateTime::from('2000-01-01'),
+            ]);
+        $this->insert('event_participant', [
+            'person_id' => end($this->persons),
+            'event_id' => $this->eventId,
+            'status' => 'applied',
+        ]);
+        $this->insert('event_person_accommodation', [
+            'person_id' => end($this->persons),
+            'event_accommodation_id' => $this->accId,
+        ]);
+    }
+
+    protected function tearDown() {
+        $this->connection->query("DELETE FROM e_dsef_participant");
+        $this->connection->query("DELETE FROM e_dsef_group");
+        $this->connection->query("DELETE FROM event_person_accommodation");
+        $this->connection->query("DELETE FROM event_accommodation");
+        parent::tearDown();
+    }
+}
