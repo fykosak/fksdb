2c8663ee36742e97fb94038f4c31d2eb0b24359d
Mišo <miso@fykos.cz>
2018-10-05 21:02:27 +0200

    add test

--- tests/Events/Accommodation/AccommodationTestCase.php
+++ tests/Events/Accommodation/AccommodationTestCase.php
@@ -88,6 +88,44 @@ EOT
         ]);
     }
 
+    protected function createAccommodationRequest() {
+        $request = $this->createPostRequest([
+            'participant' => [
+                'person_id' => "__promise",
+                'person_id_1' => [
+                    '_c_compact' => " ",
+                    'person' => [
+                        'other_name' => "František",
+                        'family_name' => "Dobrota",
+                    ],
+                    'person_info' => [
+                        'email' => "ksaadaa@kalo.cz",
+                        'id_number' => "1231354",
+                        'born' => "15. 09. 2014",
+                    ],
+                    'post_contact_p' => [
+                        'address' => [
+                            'target' => "jkljhkjh",
+                            'city' => "jkhlkjh",
+                            'postal_code' => "64546",
+                            'country_iso' => "",
+                        ],
+                    ],
+                    'person_accommodation' => [
+                        'matrix' => json_encode(['2018-06-05' => $this->accId]),
+                    ],
+                ],
+                'e_dsef_group_id' => 2,
+                'lunch_count' => 0,
+                'message' => "",
+            ],
+            'privacy' => "on",
+            'c_a_p_t_cha' => "pqrt",
+            '__init__applied' => "Přihlásit účastníka",
+        ]);
+        return $request;
+    }
+
     abstract public function getAccommodationCapacity();
 
     protected function tearDown() {
