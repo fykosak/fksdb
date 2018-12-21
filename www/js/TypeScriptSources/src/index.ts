f62664cf770a34660855a5f6c218fa3f45016974
Mišo <miso@fykos.cz>
2018-12-21 22:29:34 +0100

    fix payment

--- www/js/TypeScriptSources/src/index.ts
+++ www/js/TypeScriptSources/src/index.ts
@@ -1,10 +1,10 @@
 import { app } from './app-collector';
 import { eventAccommodation } from './events/accommodation';
 import { fyziklani } from './fyziklani';
-import { payment } from './payment/select-field';
+// import { payment } from './payment/select-field';
 
 app.register(fyziklani);
 app.register(eventAccommodation);
-app.register(payment);
+// app.register(payment);
 
 app.run();
