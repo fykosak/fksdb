f1fc572cdd1f6599834052d6958481f9f0dad2f2
Mišo <miso@fykos.cz>
2018-12-22 07:26:23 +0100

    update ACL

---
+++ app/Components/React/Fyziklani/ResultsAndStatistics/Statistics/CorrelationStatistics.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FKSDB\Components\React\Fyziklani\ResultsAndStatistics\Statistics;
+
+class CorrelationStatistics extends Statistics {
+    public function getMode(): string {
+        return 'correlation';
+    }
+}
--- app/FyziklaniModule/presenters/ResultsPresenter.php
+++ app/FyziklaniModule/presenters/ResultsPresenter.php
@@ -4,6 +4,7 @@ namespace FyziklaniModule;
 
 use FKSDB\Components\React\Fyziklani\ResultsAndStatistics\Results\ResultsPresentation;
 use FKSDB\Components\React\Fyziklani\ResultsAndStatistics\Results\ResultsView;
+use FKSDB\Components\React\Fyziklani\ResultsAndStatistics\Statistics\CorrelationStatistics;
 use FKSDB\Components\React\Fyziklani\ResultsAndStatistics\Statistics\TaskStatistics;
 use FKSDB\Components\React\Fyziklani\ResultsAndStatistics\Statistics\TeamStatistics;
 
@@ -37,6 +38,11 @@ class ResultsPresenter extends BasePresenter {
             default:
                 return true;
         }
+    }
+
+    public function titleCorrelationStatistics() {
+        $this->setTitle(_('Correlation statistics'));
+        $this->setIcon('fa fa-pie-chart');
     }
 
     public function titleDefault() {
@@ -78,6 +84,10 @@ class ResultsPresenter extends BasePresenter {
 
     public function authorizedTeamStatistics() {
         $this->authorizedDefault();
+    }
+
+    public function authorizedCorrelationStatistics() {
+        $this->setAuthorized($this->isContestsOrgAllowed('fyziklani', 'correlation'));
     }
 
     /**
@@ -123,5 +133,14 @@ class ResultsPresenter extends BasePresenter {
      */
     public function createComponentTaskStatistics(): TaskStatistics {
         return $this->fyziklaniComponentsFactory->createTaskStatistics($this->getEvent());
+    }
+
+    /**
+     * @return CorrelationStatistics
+     * @throws \Nette\Application\AbortException
+     * @throws \Nette\Application\BadRequestException
+     */
+    public function createComponentCorrelationStatistics(): CorrelationStatistics {
+        return $this->fyziklaniComponentsFactory->createCorrelationStatistics($this->getEvent());
     }
 }
