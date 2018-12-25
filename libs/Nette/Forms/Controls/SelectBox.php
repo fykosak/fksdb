520b6fd3f1cb952815ac5948fbf693f29838759e
Mišo <miso@fykos.cz>
2018-12-26 00:32:44 +0100

    fix select-boxx 2

--- libs/Nette/Forms/Controls/SelectBox.php
+++ libs/Nette/Forms/Controls/SelectBox.php
@@ -212,12 +212,12 @@ class SelectBox extends BaseControl
 
 			foreach ($value as $key2 => $value2) {
 				if ($value2 instanceof Nette\Utils\Html) {
-					$dest->addText((string) $value2->value($key2)
+					$dest->addHtml((string) $value2->value($key2)
 						->selected(isset($selected[$key2])));
 
 				} else {
 					$key2 = $this->useKeys ? $key2 : $value2;
-					$dest->addText((string) $option->value($key2)
+					$dest->addHtml((string) $option->value($key2)
 						->selected(isset($selected[$key2]))
 						->setText($this->translate((string) $value2)));
 				}
