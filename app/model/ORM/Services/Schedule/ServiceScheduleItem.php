9ad4c21e45ca85e960118bb888b0e7ade4ca74e0
Mišo <miso@fykos.cz>
2019-03-25 15:09:35 +0100

    update services schedule

--- app/model/ORM/Services/Schedule/ServiceScheduleItem.php
+++ app/model/ORM/Services/Schedule/ServiceScheduleItem.php
@@ -5,12 +5,24 @@ namespace FKSDB\ORM\Services\Schedule;
 
 use FKSDB\ORM\AbstractServiceSingle;
 use FKSDB\ORM\DbNames;
+use FKSDB\ORM\Models\Schedule\ModelScheduleItem;
 
 /**
  * Class ServiceScheduleItem
  * @package FKSDB\ORM\Services\Schedule
  */
 class ServiceScheduleItem extends AbstractServiceSingle {
-    protected $tableName = DbNames::TAB_SCHEDULE_ITEM;
-    protected $modelClassName = 'FKSDB\ORM\Models\Schedule\ModelScheduleItem';
+    /**
+     * @return string
+     */
+    protected function getModelClassName(): string {
+        return ModelScheduleItem::class;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getTableName(): string {
+        return DbNames::TAB_SCHEDULE_ITEM;
+    }
 }
