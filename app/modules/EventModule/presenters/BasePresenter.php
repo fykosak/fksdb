aedb3c454fb5e74a35fa27e18f8c210d1e97ebdb
Mišo <miso@fykos.cz>
2019-04-26 00:43:36 +0200

    fix tests

--- app/modules/EventModule/presenters/BasePresenter.php
+++ app/modules/EventModule/presenters/BasePresenter.php
@@ -4,7 +4,6 @@ namespace EventModule;
 
 use AuthenticatedPresenter;
 use FKSDB\Components\Controls\LanguageChooser;
-use FKSDB\Components\Forms\Factories\TableReflectionFactory;
 use FKSDB\ORM\Models\ModelContest;
 use FKSDB\ORM\Models\ModelEvent;
 use FKSDB\ORM\Services\ServiceContestYear;
@@ -70,13 +69,6 @@ abstract class BasePresenter extends AuthenticatedPresenter {
      */
     public function injectContainer(Container $container) {
         $this->container = $container;
-    }
-
-    /**
-     * @param TableReflectionFactory $tableReflectionFactory
-     */
-    public function injectTableReflectionFactory(TableReflectionFactory $tableReflectionFactory) {
-        $this->tableReflectionFactory = $tableReflectionFactory;
     }
 
     /**
