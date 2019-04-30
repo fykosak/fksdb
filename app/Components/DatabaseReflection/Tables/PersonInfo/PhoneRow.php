bd6953af2b8f53995d02ed4ff7c70479065c7cab
Mišo <miso@fykos.cz>
2019-05-01 00:40:28 +0200

    checkou phone numbers

--- app/Components/DatabaseReflection/Tables/PersonInfo/PhoneRow.php
+++ app/Components/DatabaseReflection/Tables/PersonInfo/PhoneRow.php
@@ -2,17 +2,44 @@
 
 namespace FKSDB\Components\DatabaseReflection\PersonInfo;
 
+use FKSDB\Components\DatabaseReflection\AbstractRow;
+use FKSDB\Components\DatabaseReflection\PhoneRowTrait;
+use FKSDB\Components\Forms\Factories\ITestedRowFactory;
+use FKSDB\ORM\Services\ServiceRegion;
+use Nette\Localization\ITranslator;
+
 /**
  * Class PhoneField
  * @package FKSDB\Components\Forms\Factories\PersonInfo
  */
-class PhoneRow extends AbstractPhoneRow {
+class PhoneRow extends AbstractRow implements ITestedRowFactory {
+    use PhoneRowTrait;
+
+    /**
+     * PhoneRow constructor.
+     * @param ServiceRegion $serviceRegion
+     * @param ITranslator $translator
+     */
+    public function __construct(ServiceRegion $serviceRegion, ITranslator $translator) {
+        parent::__construct($translator);
+        $this->serviceRegion = $serviceRegion;
+    }
+
+    /**
+     * @return string
+     */
+    public function getModelAccessKey(): string {
+        return 'phone';
+    }
+
+
     /**
      * @return string
      */
     public function getTitle(): string {
         return _('Phone number');
     }
+
     /**
      * @return int
      */
