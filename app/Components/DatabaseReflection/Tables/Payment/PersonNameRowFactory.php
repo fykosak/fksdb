71c81f72247263b085829c69412c3c7e5f051b99
Mišo <miso@fykos.cz>
2019-05-01 19:06:01 +0200

    add person link factories

---
+++ app/Components/DatabaseReflection/Tables/Payment/PersonNameRowFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace FKSDB\Components\DatabaseReflection\Payment;
+
+use FKSDB\Components\DatabaseReflection\Tables\Traits\PersonNameTrait;
+
+/**
+ * Class PersonNameRowFactory
+ * @package FKSDB\Components\DatabaseReflection\Payment
+ */
+class PersonNameRowFactory extends AbstractPaymentRow {
+    use PersonNameTrait;
+}
