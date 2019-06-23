222bcaf1d44628dd128dca343b4ba49c94f6c3d1
Mišo <miso@fykos.cz>
2019-06-23 12:45:10 +0200

    ad fyziklani game setup rows

---
+++ app/Components/DatabaseReflection/Tables/Fyziklani/FyziklaniGameSetup/GameStartRow.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace FKSDB\Components\DatabaseReflection\Fyziklani\FyziklaniGameSetup;
+
+use FKSDB\Components\DatabaseReflection\ValuePrinters\DatePrinter;
+use FKSDB\ORM\AbstractModelSingle;
+use FKSDB\ORM\Models\Fyziklani\ModelFyziklaniGameSetup;
+use Nette\Utils\Html;
+
+/**
+ * Class GameStartRow
+ * @package FKSDB\Components\DatabaseReflection\Fyziklani\FyziklaniGameSetup
+ */
+class GameStartRow extends AbstractFyziklaniGameSetupRow {
+    /**
+     * @return string
+     */
+    public function getTitle(): string {
+        return _('Game start');
+    }
+
+    /**
+     * @param AbstractModelSingle|ModelFyziklaniGameSetup $model
+     * @return Html
+     */
+    protected function createHtmlValue(AbstractModelSingle $model): Html {
+        return (new DatePrinter('d. m. Y H:i:s'))($model->game_start);
+    }
+}
