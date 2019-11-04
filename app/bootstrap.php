db325b0db6807e2c6832cbb6b36a7ecd6b95968a
Michal "Mišo" Červeňák <michalcerv363@gmail.com>
2019-11-04 19:03:00 +0100

    update bootstrap4 rendering

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,44 +4,43 @@ use Authorization\ACLExtension;
 use Events\EventsExtension;
 use FKSDB\Config\Extensions\NavigationExtension;
 use FKSDB\Config\Extensions\RouterExtension;
-//use JanTvrdik\Components\DatePicker;
+use FKSDB\Config\Extensions\StalkingExtension;
 use Kdyby\Extension\Forms\Replicator\Replicator;
 use Nette\Config\Configurator;
-use Nette\Forms\Container;
+use Nette\DI\Compiler;
 use Nette\Utils\Finder;
-use Tracy\Debugger;
 
 // Load Nette Framework
 require LIBS_DIR . '/../vendor/autoload.php';
 require LIBS_DIR . '/autoload.php';
 
-
-
-
 define('CONFIG_DIR', dirname(__FILE__) . DIRECTORY_SEPARATOR . 'config');
 
 // Configure application
 $configurator = new Configurator();
+/**
+ * @param Configurator $configurator
+ * @param Compiler $compiler
+ */
 $configurator->onCompile[] = function ($configurator, $compiler) {
-            $compiler->addExtension('fksrouter', new RouterExtension());
-            $compiler->addExtension('acl', new ACLExtension());
-            $compiler->addExtension('navigation', new NavigationExtension());
-            $compiler->addExtension('stalking', new \FKSDB\Config\Extensions\StalkingExtension());
-            $compiler->addExtension('events', new EventsExtension(CONFIG_DIR . '/events.neon'));
-        };
+    $compiler->addExtension('fksrouter', new RouterExtension());
+    $compiler->addExtension('acl', new ACLExtension());
+    $compiler->addExtension('navigation', new NavigationExtension());
+    $compiler->addExtension('stalking', new StalkingExtension());
+    $compiler->addExtension('events', new EventsExtension(CONFIG_DIR . '/events.neon'));
+};
 
 // Enable Nette Debugger for error visualisation & logging
 
-
 $configurator->enableDebugger(dirname(__FILE__) . '/../log');
-error_reporting(~E_USER_DEPRECATED&~E_USER_WARNING);
+error_reporting(~E_USER_DEPRECATED & ~E_USER_WARNING);
 
 // Enable RobotLoader - this will load all classes automatically
 $configurator->setTempDirectory(dirname(__FILE__) . '/../temp');
 $configurator->createRobotLoader()
-        ->addDirectory(APP_DIR)
-        ->addDirectory(LIBS_DIR)
-        ->register();
+    ->addDirectory(APP_DIR)
+    ->addDirectory(LIBS_DIR)
+    ->register();
 
 // Create Dependency Injection container from config.neon file
 $configurator->addConfig(CONFIG_DIR . '/config.neon', Configurator::NONE);
@@ -54,17 +53,12 @@ foreach (Finder::findFiles('*.neon')->from(dirname(__FILE__) . '/../data/events'
 
 $container = $configurator->createContainer();
 
-
 //
 // Register addons
 //
 Replicator::register();
-
-/*
-Container::extensionMethod('addDatePicker', function (Container $container, $name, $label = NULL) {
-            return $container[$name] = new DatePicker($label);
-        });
-*/
+\Tracy\Debugger::$maxLength = 100000000;
 //
 // Configure and run the application!
 $container->application->run();
+
