488902daff6e180ecb91455e335b0e6416e80e52
Michal "mišo" Červeňák <miso@fykos.cz>
2020-01-19 23:52:01 +0100

    nastrel detail

---
+++ app/Components/DatabaseReflection/DetailFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace FKSDB\Components\DatabaseReflection;
+
+use Tracy\Debugger;
+
+/**
+ * Class DetailFactory
+ * @package FKSDB\Components\DatabaseReflection
+ */
+class DetailFactory {
+    /**
+     * @var array
+     */
+    private $nodes = [];
+
+    /**
+     * @param array $nodes
+     */
+    public function setNodes(array $nodes) {
+        $this->nodes = $nodes;
+    }
+
+    /**
+     * @param string $section
+     * @return array
+     */
+    public function getSection(string $section): array {
+        return $this->nodes[$section];
+    }
+}
