e2efe40fde595587ad93ff278acb8e558aa642d1
Michal "mišo" Červeňák <miso@fykos.cz>
2020-03-01 19:31:04 +0100

    Revert "Revert "add nette tester""
    This reverts commit 1475ca6302d97b3a4591efdaff4a6d5ad7a10ae4.

--- tests/run.sh
+++ tests/run.sh
@@ -4,8 +4,8 @@ SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
 
 
 if [ "x$1" == "x" ] ; then
-	$SCRIPT_PATH/../tester/Tester/tester -p php -c $SCRIPT_PATH/php.ini $SCRIPT_PATH
+	$SCRIPT_PATH/../vendor/nette/tester/src/tester -p php -c $SCRIPT_PATH/php.ini $SCRIPT_PATH
 else
-	$SCRIPT_PATH/../tester/Tester/tester -p php -c $SCRIPT_PATH/php.ini $*
+	$SCRIPT_PATH/../vendor/nette/tester/src/tester -p php -c $SCRIPT_PATH/php.ini $*
 fi
 
