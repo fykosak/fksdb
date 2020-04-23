5fa3af23e7bf2ea8134c9b04379c6e414c1d1c3b
Michal "mišo" Červeňák <miso@fykos.cz>
2020-04-23 15:59:41 +0200

    change old WF to new

--- app/model/Authentication/AccountManager.php
+++ app/model/Authentication/AccountManager.php
@@ -100,8 +100,6 @@ class AccountManager {
      */
     public function createLoginWithInvitation(ModelPerson $person, string $email) {
         $login = $this->createLogin($person);
-        //TODO email
-        $this->serviceLogin->save($login);
 
         $until = DateTime::from($this->getInvitationExpiration());
         $token = $this->serviceAuthToken->createToken($login, ModelAuthToken::TYPE_INITIAL_LOGIN, $until);
@@ -172,21 +170,19 @@ class AccountManager {
      * @param string $login
      * @param string $password
      * @return AbstractModelSingle|ModelLogin
-     * TODO can be refactored to new WF
      */
     public final function createLogin(ModelPerson $person, string $login = null, string $password = null) {
-        $login = $this->serviceLogin->createNew([
+        /** @var ModelLogin $login */
+        $login = $this->serviceLogin->createNewModel([
             'person_id' => $person->person_id,
             'login' => $login,
             'active' => 1,
         ]);
 
-        $this->serviceLogin->save($login);
-
         /* Must be done after login_id is allocated. */
         if ($password) {
-            $login->setHash($password);
-            $this->serviceLogin->save($login);
+            $hash = $login->createHash($password);
+            $this->serviceLogin->updateModel2($login, ['hash' => $hash]);
         }
         return $login;
     }
