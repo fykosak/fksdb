146818cb2febc8fedf6be1339b2470ebf6e71019
Michal "mišo" Červeňák <miso@fykos.cz>
2020-05-07 11:15:50 +0200

    fix warnongs

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,6 +9,7 @@ use FKSDB\Config\Extensions\RouterExtension;
 use FKSDB\Config\Extensions\StalkingExtension;
 use Kdyby\Extension\Forms\Replicator\Replicator;
 use Nette\Application\Responses\TextResponse;
+use Nette\Config\Compiler;
 use Nette\Config\Configurator;
 use Nette\Utils\Finder;
 use Tester\Assert;
@@ -32,7 +33,6 @@ define('LOG_DIR', TESTS_DIR . '/../temp/tester/log');
 // Load Nette Framework
 require LIBS_DIR . '/../vendor/autoload.php';
 require LIBS_DIR . '/autoload.php';
-error_reporting(~E_USER_DEPRECATED & ~E_USER_WARNING);
 
 //require __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'tester/Tester/bootstrap.php';
 
@@ -40,7 +40,7 @@ define('CONFIG_DIR', APP_DIR . DIRECTORY_SEPARATOR . 'config');
 
 // Configure application
 $configurator = new Configurator();
-$configurator->onCompile[] = function ($configurator, $compiler) {
+$configurator->onCompile[] = function (Configurator $configurator, Compiler $compiler) {
     $compiler->addExtension('fksrouter', new RouterExtension());
     $compiler->addExtension('acl', new ACLExtension());
     $compiler->addExtension('navigation', new NavigationExtension());
@@ -56,7 +56,7 @@ Debugger::$logDirectory = LOG_DIR;
 
 // Enable RobotLoader - this will load all classes automatically
 $configurator->setTempDirectory(TEMP_DIR);
-error_reporting(~E_USER_DEPRECATED & ~E_USER_WARNING);
+error_reporting(~E_USER_DEPRECATED & ~E_USER_WARNING & ~E_WARNING & ~E_NOTICE);
 $configurator->createRobotLoader()
     ->addDirectory(APP_DIR)
     ->addDirectory(LIBS_DIR)
@@ -84,7 +84,6 @@ $container = $configurator->createContainer();
 //
 // Register addons
 //
-error_reporting(~E_USER_DEPRECATED & ~E_USER_WARNING);
 Replicator::register();
 
 function dumpResponse(TextResponse $response) {
