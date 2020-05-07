08b1f9c26e94dcb8f92c4a50ed831c8583354454
Michal "mišo" Červeňák <miso@fykos.cz>
2020-05-07 14:05:17 +0200

    remove onCompile, add extensions to conf

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,15 +1,7 @@
 <?php
 
-use Authorization\ACLExtension;
-use FKSDB\Events\EventsExtension;
-use FKSDB\Config\Extensions\DBReflectionExtension;
-use FKSDB\Config\Extensions\NavigationExtension;
-use FKSDB\Config\Extensions\PaymentExtension;
-use FKSDB\Config\Extensions\RouterExtension;
-use FKSDB\Config\Extensions\StalkingExtension;
 use Kdyby\Extension\Forms\Replicator\Replicator;
 use Nette\Configurator;
-use Nette\DI\Compiler;
 use Nette\Utils\Finder;
 
 // Load Nette Framework
@@ -19,15 +11,6 @@ define('CONFIG_DIR', dirname(__FILE__) . DIRECTORY_SEPARATOR . 'config');
 
 // Configure application
 $configurator = new Configurator();
-$configurator->onCompile[] = function (Configurator $configurator, Compiler $compiler) {
-    $compiler->addExtension('fksrouter', new RouterExtension());
-    $compiler->addExtension('acl', new ACLExtension());
-    $compiler->addExtension('navigation', new NavigationExtension());
-    $compiler->addExtension('events', new EventsExtension(CONFIG_DIR . '/events.neon'));
-    $compiler->addExtension('stalking', new StalkingExtension());
-    $compiler->addExtension('payment', new PaymentExtension());
-    $compiler->addExtension('DBReflection', new DBReflectionExtension());
-};
 
 // Enable Nette Debugger for error visualisation & logging
 $configurator->enableDebugger(dirname(__FILE__) . '/../log');
