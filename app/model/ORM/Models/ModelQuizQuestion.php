5c17a0fed7afb5340ac8e1a3aeeb985a55e1ccb2
Michal "mišo" Červeňák <miso@fykos.cz>
2020-05-12 13:41:53 +0200

    code cleanUp

--- app/model/ORM/Models/ModelQuizQuestion.php
+++ app/model/ORM/Models/ModelQuizQuestion.php
@@ -3,15 +3,16 @@
 namespace FKSDB\ORM\Models;
 
 use FKSDB\ORM\AbstractModelSingle;
+use Nette\Database\Table\ActiveRow;
 
 /**
- * 
+ *
  * @author Miroslav Jarý <mira.jary@gmail.com>
  * @property-read ActiveRow task
  * @property-read int task_id
  */
 class ModelQuizQuestion extends AbstractModelSingle implements IContestReferencedModel {
-    
+
     /**
      * (Fully qualified) question name for use in GUI
      * @return string
@@ -19,14 +20,14 @@ class ModelQuizQuestion extends AbstractModelSingle implements IContestReference
     public function getFQName(): string {
         return sprintf(_('%s. otázka'), $this->question_nr);
     }
-    
+
     /**
      * @return ModelTask
      */
     public function getTask(): ModelTask {
         return ModelTask::createFromActiveRow($this->task);
     }
-    
+
     /**
      * @return ModelContest
      */
--- app/model/ORM/Services/ServiceSubmitQuizQuestion.php
+++ app/model/ORM/Services/ServiceSubmitQuizQuestion.php
@@ -13,35 +13,32 @@ use Nette\Utils\DateTime;
  * @author Miroslav Jarý <mira.jary@gmail.com>
  */
 class ServiceSubmitQuizQuestion extends AbstractServiceSingle {
-    
+
     /** @return string */
     public function getModelClassName(): string {
         return ModelSubmitQuizQuestion::class;
     }
-    
+
     /** @return string */
     protected function getTableName(): string {
         return DbNames::TAB_SUBMIT_QUIZ;
     }
 
     /**
-     * 
+     *
      * @param int $ctId
      * @param int $questionId
      * @return ModelSubmitQuizQuestion|null
      */
     public function findByContestant(int $ctId, int $questionId) {
+        /** @var ModelSubmitQuizQuestion $result */
         $result = $this->getTable()->where([
             'ct_id' => $ctId,
             'question_id' => $questionId,
         ])->fetch();
-        if ($result) {
-            return $result;
-        } else {
-            return null;
-        }
+        return $result ?: null;
     }
-    
+
     public function saveSubmitedQuestion(ModelQuizQuestion $question, ModelContestant $contestant, string $answer) {
         $submit = $this->findByContestant($contestant->ct_id, $question->question_id);
         if ($submit) {
@@ -50,7 +47,7 @@ class ServiceSubmitQuizQuestion extends AbstractServiceSingle {
                 'answer' => $answer,
             ]);
         } else {
-            $submit = $this->createNewModel([
+            $this->createNewModel([
                 'question_id' => $question->question_id,
                 'ct_id' => $contestant->ct_id,
                 'submitted_on' => new DateTime(),
