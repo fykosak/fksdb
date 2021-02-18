5f1459a778adf33c24e9beab427709fd5955472a
Miroslav Jarý <jason@vyfuk.mff.cuni.cz>
2021-02-18 20:24:11 +0100

    Fix unreadable subtitle

--- app/Models/UI/PageTitle.php
+++ app/Models/UI/PageTitle.php
@@ -20,7 +20,7 @@ class PageTitle extends Title {
     public function toHtml(): Html {
         $container = parent::toHtml();
         if ($this->subTitle) {
-            $container->addHtml(Html::el('small')->addAttributes(['class' => 'ml-2 text-secondary small'])->addText($this->subTitle));
+            $container->addHtml(Html::el('small')->addAttributes(['class' => 'ml-2 small'])->addText($this->subTitle));
         }
         return $container;
     }
