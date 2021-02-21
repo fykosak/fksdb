4aad12b242352a734bcf2b5a108b295156054048
Matěj Rzehulka <matej.rzehulka@fykos.cz>
2021-02-21 02:06:47 +0100

    backup

--- app/Modules/EventModule/Fyziklani/templates/Diplomas/pdf-template/insertValues.js
+++ app/Modules/EventModule/Fyziklani/templates/Diplomas/pdf-template/insertValues.js
@@ -8,6 +8,21 @@ var place_tot = "25";
 var place_cat = "14";
 
 
+function addTeam(teamName, teamMembers, schoolName, lang, teamPlace_cat, teamPlace_tot){
+    if (lang == "cs"){
+        prefix = "ve složení ";
+    } else {
+        prefix = "with members ";
+    }
+    teamEnumerate = "ve složení " + teamMembers.toString() + "ze školy " + schoolName;
+    var teamName = "mujTYm";
+    var teamEnumerate = "ve složení AA bb CC";
+
+    document.getElementById('team').innerHTML = teamName;
+    document.getElementById('team_members').innerHTML = teamEnumerate;
+};
+
+
 window.onload = function(teamName, teamMembers, schoolName, lang, teamPlace_cat, teamPlace_tot, eventDate, eventName, event_website){
     // if (lang == "cs"){
     //     teamEnumerate = "ve složení " + teamMembers.toString() + "ze školy " + schoolName;
