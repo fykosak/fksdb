9b0c3496fd9a5e32aafc216b0f94c4680f2ed419
vanekm <martin.vanek42@gmail.com>
2021-03-10 18:13:19 +0100

    random pokrok v prekladech

--- app/Models/Events/Exceptions/TransitionConditionFailedException.php
+++ app/Models/Events/Exceptions/TransitionConditionFailedException.php
@@ -9,7 +9,7 @@ class TransitionConditionFailedException extends MachineExecutionException {
     private Transition $transition;
 
     public function __construct(Transition $blockingTransition, int $code = 0, ?\Throwable $previous = null) {
-        $message = sprintf(_("Nelze provést akci '%s' v automatu '%s'."), $blockingTransition->getLabel(), $blockingTransition->getBaseMachine()->getName());
+        $message = sprintf(_("Cannot carry out action '%s' in the automat '%s'."), $blockingTransition->getLabel(), $blockingTransition->getBaseMachine()->getName());
         parent::__construct($message, $code, $previous);
         $this->transition = $blockingTransition;
     }
