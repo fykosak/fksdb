18e8951c08726b15812ab3fe14f5ed094fed1afa
Michal Mišo Červeňák <miso@fykos.cz>
2021-03-24 22:46:24 +0100

    Add form adjustment

--- app/Components/Controls/Entity/TeamApplicationFormComponent.php
+++ app/Components/Controls/Entity/TeamApplicationFormComponent.php
@@ -3,8 +3,8 @@
 namespace FKSDB\Components\Controls\Entity;
 
 use FKSDB\Components\Forms\Factories\SingleReflectionFormFactory;
-use FKSDB\Models\TeamApplications\TeamApplicationHolder;
 use FKSDB\Models\TeamApplications\TeamMachine;
+use FKSDB\Models\Transitions\FormAdjustment\FormAdjustment;
 use Fykosak\NetteORM\AbstractModel;
 use Nette\DI\Container;
 use Nette\Forms\Form;
@@ -13,24 +13,19 @@ class TeamApplicationFormComponent extends AbstractEntityFormComponent {
 
     private SingleReflectionFormFactory $reflectionFormFactory;
     private TeamMachine $machine;
+    /** @var FormAdjustment[] */
     private array $adjustment;
 
     public function __construct(TeamMachine $machine, Container $container, ?AbstractModel $model) {
         parent::__construct($container, $model);
         $this->machine = $machine;
-        $this->adjustment = [
-            function (TeamApplicationHolder $holder, array $values): void {
-            },
-
-        ];
+        $this->adjustment = [];
     }
 
     protected function handleFormSuccess(Form $form): void {
         $values = $form->getValues('array');
         $holder = $this->machine->createHolder($this->model ?? null);
-        $values = array_reduce($this->adjustment, function ($prevValue, $item) {
-            return $item($this, $prevValue);
-        }, $values);
+        $values = array_reduce($this->adjustment, fn(array $prevValue, FormAdjustment $item): array => $item->adjust($prevValue, $holder), $values);
     }
 
     final public function injectPrimary(SingleReflectionFormFactory $reflectionFormFactory): void {
