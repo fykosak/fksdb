255f755f22de695eb1f3b145476642ebd7aad385
Michal Mišo Červeňák <miso@fykos.cz>
2021-03-28 04:48:16 +0200

    Fix tests

--- app/Models/Events/Model/Holder/SecondaryModelStrategies/CarefulRewrite.php
+++ app/Models/Events/Model/Holder/SecondaryModelStrategies/CarefulRewrite.php
@@ -75,6 +75,6 @@ class CarefulRewrite extends SecondaryModelStrategy {
             }
             $data[$key] = $value;
         }
-        $service->updateModelLegacy($foundModel, $data);
+        $service->updateModel($foundModel, $data);
     }
 }
