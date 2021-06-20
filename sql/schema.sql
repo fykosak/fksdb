ae4044ad1207f3ba47321f131c34f56013a9e723
Michal Mišo Červeňák <miso@fykos.cz>
2021-06-20 15:20:36 +0200

    Update submits for FOL

--- sql/schema.sql
+++ sql/schema.sql
@@ -1139,32 +1139,34 @@ CREATE TABLE IF NOT EXISTS `fyziklani_task` (
 -- -----------------------------------------------------
 -- Table `fyziklani_submit`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `fyziklani_submit` (
-  `fyziklani_submit_id` INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `fyziklani_task_id`   INT         NOT NULL,
-  `e_fyziklani_team_id` INT         NOT NULL,
-  `points`              TINYINT     NOT NULL,
-  `state`       VARCHAR(64) NULL     DEFAULT NULL,
-  `created`             DATETIME    NULL     DEFAULT CURRENT_TIMESTAMP,
-  `modified`            TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP
-  ON UPDATE CURRENT_TIMESTAMP,
-  INDEX `fk_fyziklani_submit_1_idx` (`fyziklani_task_id` ASC),
-  INDEX `fk_fyziklani_submit_2_idx` (`e_fyziklani_team_id` ASC),
-  UNIQUE INDEX `uq_fyziklani_task_id_e_fyziklani_team_id` (`fyziklani_task_id` ASC, `e_fyziklani_team_id` ASC),
+CREATE TABLE IF NOT EXISTS `fyziklani_submit`
+(
+    `fyziklani_submit_id` INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `fyziklani_task_id`   INT         NOT NULL,
+    `e_fyziklani_team_id` INT         NOT NULL,
+    `points`              TINYINT     NULL     DEFAULT NULL,
+    `skipped`             BOOLEAN     NULL     DEFAULT NULL COMMENT 'skippnutá vo FOLe',
+    `state`               VARCHAR(64) NULL     DEFAULT NULL,
+    `created`             DATETIME    NULL     DEFAULT CURRENT_TIMESTAMP,
+    `modified`            TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP
+        ON UPDATE CURRENT_TIMESTAMP,
+    INDEX `fk_fyziklani_submit_1_idx` (`fyziklani_task_id` ASC),
+    INDEX `fk_fyziklani_submit_2_idx` (`e_fyziklani_team_id` ASC),
+    UNIQUE INDEX `uq_fyziklani_task_id_e_fyziklani_team_id` (`fyziklani_task_id` ASC, `e_fyziklani_team_id` ASC),
 
-  CONSTRAINT `fk_fyziklani_submit_1`
-  FOREIGN KEY (`fyziklani_task_id`)
-  REFERENCES `fyziklani_task` (`fyziklani_task_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    CONSTRAINT `fk_fyziklani_submit_1`
+        FOREIGN KEY (`fyziklani_task_id`)
+            REFERENCES `fyziklani_task` (`fyziklani_task_id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION,
 
-  CONSTRAINT `fk_fyziklani_submit_2`
-  FOREIGN KEY (`e_fyziklani_team_id`)
-  REFERENCES `e_fyziklani_team` (`e_fyziklani_team_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
+    CONSTRAINT `fk_fyziklani_submit_2`
+        FOREIGN KEY (`e_fyziklani_team_id`)
+            REFERENCES `e_fyziklani_team` (`e_fyziklani_team_id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION
 )
-  ENGINE = 'InnoDB';
+    ENGINE = 'InnoDB';
 
 
 -- -----------------------------------------------------
