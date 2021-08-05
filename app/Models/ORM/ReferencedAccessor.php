caa2f18d49de3e96753baa13ed33dbb1f2ed900e
Michal "mišo" Červeňák <miso@fykos.cz>
2021-08-05 13:47:45 +0200

    Fix stalking add envelope link

--- app/Models/ORM/ReferencedAccessor.php
+++ app/Models/ORM/ReferencedAccessor.php
@@ -4,8 +4,10 @@ namespace FKSDB\Models\ORM;
 
 use Fykosak\NetteORM\Exceptions\CannotAccessModelException;
 use Nette\Database\Table\ActiveRow;
+use Tracy\Debugger;
 
-final class ReferencedAccessor {
+final class ReferencedAccessor
+{
 
     /**
      * @param ActiveRow $model
@@ -13,17 +15,21 @@ final class ReferencedAccessor {
      * @return ActiveRow|null
      * @throws CannotAccessModelException
      */
-    public static function accessModel(ActiveRow $model, string $modelClassName): ?ActiveRow {
+    public static function accessModel(ActiveRow $model, string $modelClassName): ?ActiveRow
+    {
         // model is already instance of desired model
         if ($model instanceof $modelClassName) {
             return $model;
         }
         $modelReflection = new \ReflectionClass($model);
         $candidates = 0;
-        $newModel = null;
         foreach ($modelReflection->getMethods() as $method) {
             $name = (string)$method->getName();
-            if ((string)$method->getReturnType() === $modelClassName) {
+            $return = (string)$method->getReturnType();
+            if (substr($return, 0, 1) == '?') {
+                $return = substr($return, 1);
+            }
+            if ($return === $modelClassName) {
                 $candidates++;
                 $newModel = $model->{$name}();
             }
