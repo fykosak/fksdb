2fe7265ffc0bfc88c0e37b76d44bc02cd1fa4535
Michal Mišo Červeňák <miso@fykos.cz>
2021-11-15 22:43:46 +0100

    Value depended toogle

---
+++ app/Models/Events/FormAdjustments/ValueDependentVisibility.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace FKSDB\Models\Events\FormAdjustments;
+
+use FKSDB\Models\Events\Model\Holder\Holder;
+use Nette\Forms\Control;
+use Nette\Forms\Controls\BaseControl;
+use Nette\Forms\Form;
+use Nette\InvalidArgumentException;
+use Tracy\Debugger;
+
+class ValueDependentVisibility extends PairwiseAdjustment
+{
+    protected function innerAdjust(Form $form, Holder $holder): void
+    {
+        foreach ($this->rules as $target => $prerequisite) {
+            Debugger::barDump($target);
+            Debugger::barDump($prerequisite);
+            $cTarget = $this->getControl($target);
+            $cPrerequisite = $this->getControl($prerequisite['from']);
+
+            if (!$cTarget || !$cPrerequisite) {
+                break;
+            }
+            if ($this->hasWildCart($target) && $this->hasWildCart($prerequisite['from'])) {
+                foreach ($cTarget as $key => $control) {
+                    if (isset($cPrerequisite[$key])) {
+                        $this->processPair($control, $cPrerequisite[$key], $prerequisite['value']);
+                    }
+                }
+            } elseif (count($cTarget) == 1) {
+                foreach ($cPrerequisite as $control) {
+                    $this->processPair(reset($cTarget), $control, $prerequisite['value']);
+                }
+            } elseif (count($cPrerequisite) == 1) {
+                foreach ($cTarget as $control) {
+                    $this->processPair($control, reset($cPrerequisite), $prerequisite['value']);
+                }
+            } else {
+                $sTarget = count($cTarget);
+                $sPrerequisite = count($cPrerequisite);
+                throw new InvalidArgumentException(
+                    "Cannot apply 1:1, 1:n, n:1 neither matching rule to '$target ($sTarget match(es)): $prerequisite ($sPrerequisite match(es))'."
+                );
+            }
+        }
+    }
+
+    /**
+     * @param null $value
+     */
+    protected function processPair(BaseControl $target, Control $prerequisite, $value = null): void
+    {
+        $target->getRules()->addConditionOn($prerequisite, Form::NOT_EQUAL, $value)->toggle($target->getHtmlId() . '-pair');
+    }
+}
