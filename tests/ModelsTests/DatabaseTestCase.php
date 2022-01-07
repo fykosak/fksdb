cfdf1e443a92e36020e3671a807bb5347b753d89
Michal Mišo Červeňák <miso@fykos.cz>
2022-01-07 02:06:31 +0100

    Revert "fix tests"
    This reverts commit ff9bfe28f37cec32cabdd1709e5d1bc5d043f713.

--- tests/ModelsTests/DatabaseTestCase.php
+++ tests/ModelsTests/DatabaseTestCase.php
@@ -42,24 +42,19 @@ abstract class DatabaseTestCase extends TestCase
 
     protected function setUp(): void
     {
-        // TODO complete refactoring!!!!!
         Environment::lock(LOCK_DB . $this->instanceNo, TEMP_DIR);
         $this->explorer->query(
-            "INSERT INTO address (address_id, target, city, region_id) VALUES(1, 'nikde', 'nicov', 3) 
-ON DUPLICATE KEY UPDATE address_id=address_id"
+            "INSERT INTO address (address_id, target, city, region_id) VALUES(1, 'nikde', 'nicov', 3)"
         );
         $this->explorer->query(
-            "INSERT INTO school (school_id, name, name_abbrev, address_id) VALUES(1, 'Skola', 'SK', 1)  
-ON DUPLICATE KEY UPDATE school_id=school_id"
+            "INSERT INTO school (school_id, name, name_abbrev, address_id) VALUES(1, 'Skola', 'SK', 1)"
         );
         $this->explorer->query(
-            "INSERT INTO contest_year (contest_id, year, ac_year) VALUES(1, 1, ?) 
-ON DUPLICATE KEY UPDATE contest_id=contest_id",
+            "INSERT INTO contest_year (contest_id, year, ac_year) VALUES(1, 1, ?)",
             YearCalculator::getCurrentAcademicYear()
         );
         $this->explorer->query(
-            "INSERT INTO contest_year (contest_id, year, ac_year) VALUES(2, 1, ?) 
-ON DUPLICATE KEY UPDATE contest_id=contest_id",
+            "INSERT INTO contest_year (contest_id, year, ac_year) VALUES(2, 1, ?)",
             YearCalculator::getCurrentAcademicYear()
         );
     }
