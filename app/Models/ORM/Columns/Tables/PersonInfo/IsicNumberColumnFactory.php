f1e9afea30077ca6c033637a760ba60310da4dce
Adam Krška <adam@krska.com>
2022-07-16 01:20:45 +0200

    fix codestyle

--- app/Models/ORM/Columns/Tables/PersonInfo/IsicNumberColumnFactory.php
+++ app/Models/ORM/Columns/Tables/PersonInfo/IsicNumberColumnFactory.php
@@ -18,7 +18,11 @@ class IsicNumberColumnFactory extends ColumnFactory
                     return str_replace(' ', '', $value); // remove whitespaces
         })
             ->addRule(Form::LENGTH, _('ISIC must start and end with a capital letter and contain 12 digits'), 14)
-            ->addRule(Form::PATTERN, _('ISIC must start and end with a capital letter and contain 12 digits'), '^[A-Z]\d{12}[A-Z]$');
+            ->addRule(
+                Form::PATTERN,
+                _('ISIC must start and end with a capital letter and contain 12 digits'),
+                '^[A-Z]\d{12}[A-Z]$'
+            );
         return $control;
     }
 }
