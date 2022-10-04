0b8f7d2832d0e3e527afe5808e3c2c27c1e8d66c
Michal Červeňák <miso@fykos.cz>
2022-10-04 22:36:47 +0200

    Update TeamMemberModel.php

--- app/Models/ORM/Models/Fyziklani/TeamMemberModel.php
+++ app/Models/ORM/Models/Fyziklani/TeamMemberModel.php
@@ -11,8 +11,8 @@ use Fykosak\NetteORM\Model;
 
 /**
  * @property-read int fyziklani_team_member_id
- * @property-read PersonModel person
  * @property-read int person_id
+ * @property-read PersonModel person
  * @property-read int fyziklani_team_id
  * @property-read TeamModel2 fyziklani_team
  */
