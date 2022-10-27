1125e8fd9755f519ca2be4c8cdd8522807c3b768
Adam Krška <adam@krska.com>
2022-10-27 11:53:37 +0200

    add setkani callback

---
+++ app/Models/Transitions/Callbacks/Setkani/ParticipantMailCallback.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FKSDB\Models\Transitions\Callbacks\Setkani;
+
+use FKSDB\Models\Transitions\Callbacks\EventParticipantCallback;
+use FKSDB\Models\Transitions\Holder\ModelHolder;
+
+class ParticipantMailCallback extends EventParticipantCallback
+{
+
+    protected function getTemplatePath(ModelHolder $holder): string
+    {
+        return __DIR__ . DIRECTORY_SEPARATOR . 'confirmation';
+    }
+
+    protected function getData(ModelHolder $holder): array
+    {
+        return [
+            'subject' => 'Výfučí setkání',
+            'blind_carbon_copy' => 'Výfučí setkání <vyfuk@vyfuk.mff.cuni.cz>',
+            'sender' => 'Výfučí setkání <vyfuk@vyfuk.mff.cuni.cz>',
+        ];
+    }
+}
