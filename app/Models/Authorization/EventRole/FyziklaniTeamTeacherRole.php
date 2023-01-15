c8bdc075b4b9967a58afc4f12b7cb2452daf1d80
Michal Mišo Červeňák <miso@fykos.cz>
2023-01-15 23:06:48 +0100

    role printer

--- app/Models/Authorization/EventRole/FyziklaniTeamTeacherRole.php
+++ app/Models/Authorization/EventRole/FyziklaniTeamTeacherRole.php
@@ -6,6 +6,7 @@ namespace FKSDB\Models\Authorization\EventRole;
 
 use FKSDB\Models\ORM\Models\Fyziklani\TeamModel2;
 use FKSDB\Models\ORM\Models\EventModel;
+use Nette\Utils\Html;
 
 class FyziklaniTeamTeacherRole extends EventRole
 {
@@ -16,5 +17,14 @@ class FyziklaniTeamTeacherRole extends EventRole
     {
         parent::__construct('event.fyziklaniTeamTeacher', $event);
         $this->teams = $teams;
+    }
+
+    public function badge(): Html
+    {
+        $container = Html::el('span')->addText(_('Teacher: '))->addAttributes(['class' => 'badge bg-color-5']);
+        foreach ($this->teams as $key => $team) {
+            $container->addText(($key === 0 ? '' : ', ') . $team->name . ' (' . $team->state->label() . ')');
+        }
+        return $container;
     }
 }
