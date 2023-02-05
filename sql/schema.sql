fb50928feb881f7ebc9176b1c42aa0a84d10bcb6
vercah <vercaposta@seznam.cz>
2023-02-05 16:15:32 +0100

    change row to varchar

--- sql/schema.sql
+++ sql/schema.sql
@@ -1069,7 +1069,7 @@ CREATE TABLE IF NOT EXISTS `fyziklani_seat`
     `fyziklani_seat_id` INT        NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `fyziklani_room_id` INT        NOT NULL,
     `sector`            VARCHAR(2) NULL DEFAULT NULL,
-    `row`               SMALLINT   NULL DEFAULT NULL,
+    `row`               VARCHAR(2) NULL DEFAULT NULL,
     `col`               SMALLINT   NULL DEFAULT NULL,
     `layout_x`          DOUBLE     NOT NULL,
     `layout_y`          DOUBLE     NOT NULL,
