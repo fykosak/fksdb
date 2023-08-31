745fb6edf05099994e93d37499a75297c12cd730
kalavaros <marek.milicka@gmail.com>
2023-08-31 16:37:13 +0200

    working barcode coded solutions

--- app/Models/Submits/PDFStamper.php
+++ app/Models/Submits/PDFStamper.php
@@ -6,6 +6,7 @@ namespace FKSDB\Models\Submits;
 use FKSDB\Models\ORM\Models\SubmitModel;
 use Nette\InvalidStateException;
 use Nette\Utils\Strings;
+use Tracy\Debugger;
 
 class PDFStamper implements StorageProcessing
 {
@@ -63,7 +64,7 @@ class PDFStamper implements StorageProcessing
         $label = $submit->task->label;
         $person = $submit->contestant->person;
 
-        $stampText = sprintf('S%dU%s, %s, %s', $series, $label, $person->getFullName(), $submit->submit_id);
+        $stampText = sprintf('S%dU%sI%sSB%s', $series, $label, $person->person_id, $submit->submit_id);
         try {
             $this->stampText($stampText);
         } catch (\Throwable $exception) {
@@ -75,23 +76,26 @@ class PDFStamper implements StorageProcessing
     {
         $pdf = new \setasign\Fpdi\Fpdi();
         $pageCount = $pdf->setSourceFile($this->getInputFile());
+        $generator = new \Picqer\Barcode\BarcodeGeneratorJPG();
         
         for ($page = 1; $page <= $pageCount; ++$page) {
             $tpl = $pdf->importPage($page);
-            $actText = $text . ' page ' . $page . '/' . $pageCount;
+            $actText = $text . 'P' . $page . '/' . $pageCount;
+            $barcode = $generator->getBarcode($actText, $generator::TYPE_CODE_128);
+            file_put_contents('barcode.jpg', $barcode);
             $specs = $pdf->getTemplateSize($tpl);
             
             $orientation = $specs['orientation'];
             $pdf->AddPage($orientation);
             $pdf->useTemplate($tpl, 1, 1, null, null, true);
-
+            /*
             // calculate size of the stamp
             $pdf->SetFont('Courier', 'b', $this->getFontSize());
             $pdf->SetDrawColor(0, 0, 0);// @phpstan-ignore-line
             $pw = 210; // pagewidth, A4 210 mm
             $offset = 7; // vertical offset
             $tw = $pdf->GetStringWidth($actText); // @phpstan-ignore-line
-            $th = $this->getFontSize() * 0.35; // 1pt = 0.35mm
+            $th = $this->getFontSize() * 0.35; // 1pt = 0.35mm <-wtf
             $x = ($pw - $tw) / 2;
             $y = $th + $offset;
             // stamp background
@@ -104,9 +108,31 @@ class PDFStamper implements StorageProcessing
                 ($th + 2 * $margin),
                 'F'
             );
+            */
 
-            $stampText = Strings::webalize($actText, ' ,.', false); // FPDF has only ASCII encoded fonts
-            $pdf->Text($x, $y, $stampText);// @phpstan-ignore-line
+            //calculate barcode info
+            $pw = 210; // pagewidth, A4 210 mm
+            $offset = 7; // vertical offset
+            $bcSize = getimagesize('barcode.jpg');
+            $bcw = $bcSize[0]*0.35;
+            $bch = $bcSize[1]*0.35;
+            $x = ($pw - $bcw) / 2;
+            $y = $offset; //for readibility
+
+            // stamp background
+            $margin = 2;
+            $pdf->SetFillColor(240, 240, 240); // @phpstan-ignore-line
+            $pdf->Rect( // @phpstan-ignore-line
+                $x - $margin,
+                $y - $th - $margin,
+                $bcw + 2 * $margin,
+                ($bch + 2 * $margin),
+                'F'
+            );
+            Debugger::log($x);
+            //stampText = Strings::webalize($actText, ' ,.', false); // FPDF has only ASCII encoded fonts
+            $pdf->Image('barcode.jpg', $x, $y);// @phpstan-ignore-line
+
         }
 
         $pdf->Output($this->getOutputFile(), 'F');// @phpstan-ignore-line
