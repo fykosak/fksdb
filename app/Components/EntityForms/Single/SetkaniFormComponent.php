a75945eab1a1485ea5d70fd63a49c0716686815e
Adam Krška <adam@krska.com>
2023-10-16 17:16:54 +0200

    add pending state

--- app/Components/EntityForms/Single/SetkaniFormComponent.php
+++ app/Components/EntityForms/Single/SetkaniFormComponent.php
@@ -5,12 +5,7 @@ declare(strict_types=1);
 namespace FKSDB\Components\EntityForms\Single;
 
 use FKSDB\Components\Forms\Containers\Models\ReferencedPersonContainer;
-use FKSDB\Models\ORM\DbNames;
-use FKSDB\Models\ORM\Models\ContestantModel;
-use FKSDB\Models\ORM\Models\PersonModel;
-use FKSDB\Models\ORM\Services\PersonService;
 use FKSDB\Modules\Core\BasePresenter;
-use Nette\Forms\Form;
 use Nette\Neon\Exception;
 
 /**
@@ -19,12 +14,6 @@ use Nette\Neon\Exception;
  */
 final class SetkaniFormComponent extends SingleFormComponent
 {
-
-    private PersonService $personService;
-
-    public function injectPersonService(PersonService $personService): void {
-        $this->personService = $personService;
-    }
 
     /**
      * @throws Exception
@@ -75,42 +64,5 @@ final class SetkaniFormComponent extends SingleFormComponent
                 ohledně tvého zdravotního stavu, co bychom měli vědět?")],
             'note' => ['required' => false]
         ];
-    }
-
-    /**
-     * @throws \Throwable
-     */
-    protected function handleFormSuccess(Form $form): void
-    {
-        /** @var array<string,mixed> $values */
-        $values = $form->getValues('array');
-
-        /** @var PersonModel|Null */
-        $personModel = $this->personService->findByPrimary($values['person_id']);
-
-        if (!$personModel) {
-            throw new NotContestantException();
-        }
-
-        /** @var int $contestYear */
-        $contestYear = $this->event->getContestYear()->year;
-
-        /** @var array<ContestantModel> $contestantModels */
-        $contestantModels = $personModel->getContestants($this->event->event_type->contest)->where('year', [$contestYear, $contestYear-1]);
-
-        if(count($contestantModels) == 0) {
-            throw new NotContestantException();
-        }
-
-        $submitsCount = 0;
-        foreach ($contestantModels as $contestant) {
-            $submitsCount += $contestant->getSubmits()->count();
-        }
-
-        if ($submitsCount == 0) {
-            throw new NotContestantException();
-        }
-
-        parent::handleFormSuccess($form);
     }
 }
