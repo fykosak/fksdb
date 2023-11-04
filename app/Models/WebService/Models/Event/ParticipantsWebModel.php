55c0a178c6f589ad71b4bc1d63511e6016029561
Adam Krška <adam@krska.com>
2023-11-04 13:45:22 +0100

    export participant status

--- app/Models/WebService/Models/Event/ParticipantsWebModel.php
+++ app/Models/WebService/Models/Event/ParticipantsWebModel.php
@@ -48,6 +48,7 @@ class ParticipantsWebModel extends WebModel
             $school = $history->school;
             $data[] = array_merge($participant->person->__toArray(), [
                 'eventParticipantId' => $participant->event_participant_id,
+                'status' => $participant->status->value,
                 'lunchCount' => $participant->lunch_count ?? 0,
                 'code' => $participant->createMachineCode(),
                 'school' => $school ? $school->__toArray() : null,
