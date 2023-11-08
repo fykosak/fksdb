bf9d14097c5b365ef66109c27d81738e00976227
Michal Mišo Červeňák <miso@fykos.cz>
2023-11-09 00:26:31 +0100

    eventId as param

--- tools/event-report.php
+++ tools/event-report.php
@@ -8,21 +8,16 @@ use Fykosak\Utils\Logging\MemoryLogger;
 use Fykosak\Utils\Logging\Message;
 use Nette\DI\Container;
 
-const SAFE_LIMIT = 500;
-
 /** @var Container $container */
 $container = require __DIR__ . '/bootstrap.php';
 
-$eventIds = [173];
-set_time_limit(120);
+set_time_limit(-1);
 $service = $container->getByType(EventService::class);
 $dataTestFactory = $container->getByType(DataTestFactory::class);
 $tests = $dataTestFactory->getEventTests();
 $logger = new MemoryLogger();
-foreach ($eventIds as $eventId) {
-    $event = $service->findByPrimary($eventId);
-    foreach ($tests as $test) {
-        $test->run($logger, $event);
-    }
+$event = $service->findByPrimary(+$argv[1]);
+foreach ($tests as $test) {
+    $test->run($logger, $event);
 }
 echo json_encode(array_map(fn(Message $message) => $message->__toArray(), $logger->getMessages()));
