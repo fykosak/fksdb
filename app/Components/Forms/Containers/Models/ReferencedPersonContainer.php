aecd8b477e6a299f8546939b3e6acc0562fa1a62
Michal Mišo Červeňák <miso@fykos.cz>
2023-11-22 15:43:39 +0100

    PHPStan

--- app/Components/Forms/Containers/Models/ReferencedPersonContainer.php
+++ app/Components/Forms/Containers/Models/ReferencedPersonContainer.php
@@ -87,9 +87,9 @@ class ReferencedPersonContainer extends ReferencedContainer
     {
         foreach ($this->fieldsDefinition as $sub => $fields) {
             $subContainer = new ContainerWithOptions($this->container);
-            if ($sub === ReferencedPersonHandler::POST_CONTACT_DELIVERY) {
+            if ($sub === ReferencedPersonHandler::POST_CONTACT_DELIVERY) { // @phpstan-ignore-line
                 $subContainer->setOption('label', _('Deliver address'));
-            } elseif ($sub === ReferencedPersonHandler::POST_CONTACT_PERMANENT) {
+            } elseif ($sub === ReferencedPersonHandler::POST_CONTACT_PERMANENT) { // @phpstan-ignore-line
                 $label = _('Permanent address');
                 if ($this->getComponent(ReferencedPersonHandler::POST_CONTACT_DELIVERY, false)) {
                     $label .= ' ' . _('(when different from delivery address)');
@@ -97,8 +97,8 @@ class ReferencedPersonContainer extends ReferencedContainer
                 $subContainer->setOption('label', $label);
             }
             if (
-                $sub === ReferencedPersonHandler::POST_CONTACT_DELIVERY ||
-                $sub === ReferencedPersonHandler::POST_CONTACT_PERMANENT
+                $sub === ReferencedPersonHandler::POST_CONTACT_DELIVERY || // @phpstan-ignore-line
+                $sub === ReferencedPersonHandler::POST_CONTACT_PERMANENT // @phpstan-ignore-line
             ) {
                 if (isset($fields['address'])) {
                     $control = new AddressDataContainer(
