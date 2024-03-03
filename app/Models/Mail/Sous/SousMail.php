d0a0b65995d75bec2d411c8340f577b8017f0869
Michal Mišo Červeňák <miso@fykos.cz>
2024-03-03 01:09:07 +0100

    remove old neon + move email to Mail folder

--- app/Models/Mail/Sous/SousMail.php
+++ app/Models/Mail/Sous/SousMail.php
@@ -2,34 +2,27 @@
 
 declare(strict_types=1);
 
-namespace FKSDB\Models\Transitions\Callbacks\Sous;
+namespace FKSDB\Models\Mail\Sous;
 
 use FKSDB\Models\Events\Model\Holder\BaseHolder;
 use FKSDB\Models\Transitions\Callbacks\EventParticipantCallback;
 use FKSDB\Models\Transitions\Holder\ModelHolder;
-use FKSDB\Models\Transitions\Transition\Transition;
 
 /**
  * @phpstan-extends EventParticipantCallback<BaseHolder>
  */
-class Reminder1MailCallback extends EventParticipantCallback
+abstract class SousMail extends EventParticipantCallback
 {
-    protected function getTemplatePath(ModelHolder $holder, Transition $transition): string
-    {
-        return __DIR__ . DIRECTORY_SEPARATOR . 'reminder1.latte';
-    }
-
     /**
      * @phpstan-return array{
      *     blind_carbon_copy?:string,
-     *     subject:string,
      *     sender:string,
      * }
      */
-    protected function getData(ModelHolder $holder): array
+    final protected function getData(ModelHolder $holder): array
     {
         return [
-            'subject' => 'Pozvánka na jarní soustředění FYKOSu',
+            'blind_carbon_copy' => 'Soustředění FYKOSu <soustredeni@fykos.cz>',
             'sender' => 'Soustředění FYKOSu <soustredeni@fykos.cz>',
         ];
     }
