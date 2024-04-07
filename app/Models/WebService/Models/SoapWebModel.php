a001be139c4cd10f21af9e6129c2dc74475410b9
Michal Mišo Červeňák <miso@fykos.cz>
2024-04-07 18:02:35 +0200

    SPR12

--- app/Models/WebService/Models/SoapWebModel.php
+++ app/Models/WebService/Models/SoapWebModel.php
@@ -4,12 +4,7 @@ declare(strict_types=1);
 
 namespace FKSDB\Models\WebService\Models;
 
-use FKSDB\Models\Exceptions\GoneException;
-
 interface SoapWebModel
 {
-    /**
-     * @throws GoneException
-     */
     public function getResponse(\stdClass $args): \SoapVar;
 }
