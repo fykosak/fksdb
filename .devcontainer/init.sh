2b26e6fbe1e5d23b60581ec230c83f44e48bda27
Pavel Provazník <41260648+paproc@users.noreply.github.com>
2024-07-05 17:25:28 +0200

    docker

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -5,6 +5,5 @@ composer install
 npm install
 npm run build
 cp .devcontainer/config.local.neon app/config/config.local.neon
-cat sql/schema.sql sql/views.sql sql/initval.sql sql/stored_query.sql | mysql -h 127.0.0.1 -u fksdb -pfksdb fksdb
 i18n/compile.sh
 rm -rf temp/cache
