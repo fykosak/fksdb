422b55c4f1831fba4223c9dc9cbb99ea61b3f738
Michal Mišo Červeňák <miso@fykos.cz>
2024-07-13 20:23:12 +0200

    clen bootstrap

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -32,10 +32,6 @@ class Bootstrap
         $configurator->addConfig(__DIR__ . '/config/config.neon');
         $configurator->addConfig(__DIR__ . '/config/config.local.neon');
 
-        // Load all .neon files in events data directory
-        // foreach (Finder::findFiles('*.neon')->from(__DIR__ . '/../data/events') as $filename => $file) {
-        // $configurator->addConfig($filename);
-        // }
         return $configurator;
     }
 }
