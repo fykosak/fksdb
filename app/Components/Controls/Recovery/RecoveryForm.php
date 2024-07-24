4ad4e1501bcf144708a3ec1eafe43501d9bb011b
Michal Mišo Červeňák <miso@fykos.cz>
2024-07-24 21:11:13 +0200

    Login & recovery to separate form

---
+++ app/Components/Controls/Recovery/RecoveryForm.php
@@ -0,0 +1,101 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FKSDB\Components\Controls\Recovery;
+
+use FKSDB\Components\Controls\FormComponent\FormComponent;
+use FKSDB\Models\Authentication\Exceptions\RecoveryException;
+use FKSDB\Models\Authentication\Exceptions\RecoveryExistsException;
+use FKSDB\Models\Authentication\PasswordAuthenticator;
+use FKSDB\Models\Email\Source\PasswordRecovery\PasswordRecoveryEmailSource;
+use FKSDB\Models\Exceptions\BadTypeException;
+use FKSDB\Models\ORM\Models\AuthTokenModel;
+use FKSDB\Models\ORM\Models\AuthTokenType;
+use FKSDB\Models\ORM\Services\AuthTokenService;
+use FKSDB\Models\Utils\Utils;
+use FKSDB\Modules\Core\Language;
+use Fykosak\Utils\Logging\Message;
+use Nette\Application\BadRequestException;
+use Nette\Forms\Controls\SubmitButton;
+use Nette\Forms\Form;
+use Nette\Security\AuthenticationException;
+use Nette\Utils\DateTime;
+
+class RecoveryForm extends FormComponent
+{
+    private AuthTokenService $authTokenService;
+    private PasswordAuthenticator $passwordAuthenticator;
+
+    final public function inject(
+        AuthTokenService $authTokenService,
+        PasswordAuthenticator $passwordAuthenticator
+    ): void {
+        $this->authTokenService = $authTokenService;
+        $this->passwordAuthenticator = $passwordAuthenticator;
+    }
+
+    protected function getTemplatePath(): string
+    {
+        return __DIR__ . '/layout.latte';
+    }
+
+    /**+
+     * @throws BadRequestException
+     * @throws BadTypeException
+     */
+    protected function handleSuccess(Form $form): void
+    {
+        $connection = $this->authTokenService->explorer->getConnection();
+        try {
+            /**
+             * @phpstan-var array{id:string} $values
+             */
+            $values = $form->getValues('array');
+            $connection->beginTransaction();
+            $login = $this->passwordAuthenticator->findLogin($values['id']);
+            /** @var AuthTokenModel|null $token */
+            $token = $login->getActiveTokens(AuthTokenType::from(AuthTokenType::RECOVERY))->fetch();
+            if ($token) {
+                throw new RecoveryExistsException();
+            }
+
+            $until = DateTime::from($this->getContext()->getParameters()['recovery']['expiration']);
+            $token = $this->authTokenService->createToken($login, AuthTokenType::from(AuthTokenType::RECOVERY), $until);
+
+            $person = $login->person;
+            if (!$person) {
+                throw new BadRequestException();
+            }
+            $source = new PasswordRecoveryEmailSource($this->getContext());
+            $source->createAndSend([
+                'token' => $token,
+                'person' => $person,
+                'lang' => Language::from($login->person->getPreferredLang() ?? $this->translator->lang),
+            ]);
+
+            $email = Utils::cryptEmail($login->person->getInfo()->email);
+            $this->getPresenter()->flashMessage(
+                sprintf(_('Further instructions for the recovery have been sent to %s.'), $email),
+                Message::LVL_SUCCESS
+            );
+            $connection->commit();
+            $this->getPresenter()->redirect('login');
+        } catch (AuthenticationException|RecoveryException $exception) {
+            $this->getPresenter()->flashMessage($exception->getMessage(), Message::LVL_ERROR);
+            $connection->rollBack();
+        }
+    }
+
+    protected function appendSubmitButton(Form $form): SubmitButton
+    {
+        return $form->addSubmit('send', _('Continue'));
+    }
+
+    protected function configureForm(Form $form): void
+    {
+        $form->addText('id', _('Login or e-mail address'))
+            ->addRule(Form::FILLED, _('Insert login or email address.'));
+        $form->addProtection(_('The form has expired. Please send it again.'));
+    }
+}
--- app/Models/Authentication/AccountManager.php
+++ app/Models/Authentication/AccountManager.php
@@ -4,19 +4,14 @@ declare(strict_types=1);
 
 namespace FKSDB\Models\Authentication;
 
-use FKSDB\Models\Authentication\Exceptions\RecoveryExistsException;
-use FKSDB\Models\Authentication\Exceptions\RecoveryNotImplementedException;
 use FKSDB\Models\Email\Source\LoginInvitation\LoginInvitationEmailSource;
-use FKSDB\Models\Email\Source\PasswordRecovery\PasswordRecoveryEmailSource;
 use FKSDB\Models\Exceptions\BadTypeException;
-use FKSDB\Models\ORM\Models\AuthTokenModel;
 use FKSDB\Models\ORM\Models\AuthTokenType;
 use FKSDB\Models\ORM\Models\LoginModel;
 use FKSDB\Models\ORM\Models\PersonModel;
 use FKSDB\Models\ORM\Services\AuthTokenService;
 use FKSDB\Models\ORM\Services\LoginService;
 use FKSDB\Modules\Core\Language;
-use Nette\Application\BadRequestException;
 use Nette\DI\Container;
 use Nette\SmartObject;
 use Nette\Utils\DateTime;
@@ -26,20 +21,17 @@ class AccountManager
     use SmartObject;
 
     private string $invitationExpiration;
-    private string $recoveryExpiration;
     private LoginService $loginService;
     private AuthTokenService $authTokenService;
     private Container $container;
 
     public function __construct(
         string $invitationExpiration,
-        string $recoveryExpiration,
         Container $container,
         LoginService $loginService,
         AuthTokenService $authTokenService
     ) {
         $this->invitationExpiration = $invitationExpiration;
-        $this->recoveryExpiration = $recoveryExpiration;
         $this->loginService = $loginService;
         $this->authTokenService = $authTokenService;
         $this->container = $container;
@@ -67,35 +59,5 @@ class AccountManager
             'lang' => Language::from($person->getPreferredLang() ?? $lang->value),
         ]);
         return $login;
-    }
-
-    /**
-     * @throws BadTypeException
-     * @throws \Exception
-     */
-    public function sendRecovery(LoginModel $login, Language $lang): void
-    {
-        if (!$login->person_id) {
-            throw new RecoveryNotImplementedException();
-        }
-        /** @var AuthTokenModel|null $token */
-        $token = $login->getActiveTokens(AuthTokenType::from(AuthTokenType::RECOVERY))->fetch();
-        if ($token) {
-            throw new RecoveryExistsException();
-        }
-
-        $until = DateTime::from($this->recoveryExpiration);
-        $token = $this->authTokenService->createToken($login, AuthTokenType::from(AuthTokenType::RECOVERY), $until);
-
-        $person = $login->person;
-        if (!$person) {
-            throw new BadRequestException();
-        }
-        $source = new PasswordRecoveryEmailSource($this->container);
-        $source->createAndSend([
-            'token' => $token,
-            'person' => $person,
-            'lang' => $lang,
-        ]);
     }
 }
